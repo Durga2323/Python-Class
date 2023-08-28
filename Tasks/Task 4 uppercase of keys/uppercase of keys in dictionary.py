dd6780eb137796c59e80044039153edec66b057b
Durgarao Bogavalli <bogavalli.vvsdurgarao23@gmail.com>
2023-08-28 18:17:58 +0530

    sir explanation

--- Tasks/Task 4 uppercase of keys/uppercase of keys in dictionary.py
+++ Tasks/Task 4 uppercase of keys/uppercase of keys in dictionary.py
@@ -1,34 +1,34 @@
-dict1={
-    "asaa":"asaa",
-    "fsaf":"fsaf",
-    "gagd":{
-        "data":"data",
-        "value":"value",
-        "info":{
-            "compress":"compress",
-            "product":"product",
-            "data1":{
-                "goon":"goon"
-            }
-        }
-    },
-    "sample":"sample"
-}
+# dict1={
+#     "asaa":"asaa",
+#     "fsaf":"fsaf",
+#     "gagd":{
+#         "data":"data",
+#         "value":"value",
+#         "info":{
+#             "compress":"compress",
+#             "product":"product",
+#             "data1":{
+#                 "goon":"goon"
+#             }
+#         }
+#     },
+#     "sample":"sample"
+# }
 
 # -----------------------------------------
 
-def convert_keys_to_uppercase(dictionary):
-    new_dict = { }
-    for key,value in dictionary.items():
-        if isinstance(value,dict):
-            new_dict[key.upper()]=convert_keys_to_uppercase(value)
-        else:
-            new_dict[key.upper()]=value
-    return new_dict
+# def convert_keys_to_uppercase(dictionary):
+#     new_dict = { }
+#     for key,value in dictionary.items():
+#         if isinstance(value,dict):
+#             new_dict[key.upper()]=convert_keys_to_uppercase(value)
+#         else:
+#             new_dict[key.upper()]=value
+#     return new_dict
 
-dict_upper_keys=convert_keys_to_uppercase(dict1)
+# dict_upper_keys=convert_keys_to_uppercase(dict1)
 
-print(dict_upper_keys)
+# print(dict_upper_keys)
 
 # ------------------------------------
 # key1 = dict1.keys()
@@ -67,9 +67,35 @@ print(dict_upper_keys)
 # print(up_keys(dict1))
 
 
+#--------------------------------------------------------------------
+dict1={
+    "asaa":"asaa",
+    "fsaf":"fsaf",
+    "gagd":{
+        "data":"data",
+        "value":"value",
+        "info":{
+            "compress":"compress",
+            "product":"product",
+            "data1":{
+                "goon":"goon"
+            }
+        }
+    },
+    "sample":"sample"
+}
 
 
+def sample_dict(dict1):
+    dict2={}
+    for ele in dict1:
+        if type(dict1[ele]) == dict:
+            dict2[ele.upper()]=sample_dict(dict1[ele])
+        else:
+            dict2[ele.upper()] = dict1[ele]
+    return dict2
 
+print(sample_dict(dict1))
 
 
 
